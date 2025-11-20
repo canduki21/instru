2fb01d2ebf2699e94bb22921295da480c2c9299d
canduki21 <cacndela@gmail.com>
2025-11-20 17:42:22 -0500

    Update TCPCommunicatorClient.py

--- TCPCommunicatorClient.py
+++ TCPCommunicatorClient.py
@@ -7,49 +7,36 @@ import select
 from time import sleep
 
 class Transmit(threading.Thread):
-	def run(self):
-		# Sending data to the server
-		#print('Transmit started')
-		global s # connection
-		global threadRunning
-		while(threadRunning):
-			# Read text with 1 sec timeout
-			i, o, e = select.select( [sys.stdin], [], [], 1 )
-			if(i):
-				string = sys.stdin.readline().strip() + '\r\n'
-				byteArray = bytearray(string, "utf-8") # Convert string into a b$
-				s.sendall(byteArray)
-
-		#print('Transmit stopped - threadRunning')
-		return
+    def run(self):
+        global s
+        global threadRunning
+        while(threadRunning):
+            i, o, e = select.select([sys.stdin], [], [], 1)
+            if(i):
+                string = sys.stdin.readline().strip() + '\r\n'
+                byteArray = bytearray(string, "utf-8")
+                s.sendall(byteArray)
+        return
 
 class Receive(threading.Thread):
-	def run(self):
-		# Receiving data from the server
-		#print('Receive started')
-		global s # connection
-		global threadRunning
-		while(threadRunning):
-			# Use 1 second timeout on receive
-			ready = select.select( [s], [], [], 1 )
-			if(ready[0]):
-				data = s.recv(1024).strip()
-				if(len(data) == 0): # Connection closed by server
-					threadRunning = False
-				else:
-					print('Received:', data.decode("utf-8")) # Converts thhe receive$
+    def run(self):
+        global s
+        global threadRunning
+        while(threadRunning):
+            ready = select.select([s], [], [], 1)
+            if(ready[0]):
+                data = s.recv(1024).strip()
+                if(len(data) == 0):
+                    threadRunning = False
+                else:
+                    print('Received:', data.decode("utf-8"))
+            sleep(0.1)
+        return
 
-			sleep(0.1)
+HOST = '192.168.1.72'   # <--- CHANGE TO WINDOWS IP
+PORT = 5000             # <--- CHANGE TO YOUR LABVIEW PORT
 
-		#print('Receive stopped - threadRunning')
-		return
-
-# Running main program
-HOST = '10.0.0.245' # The remote host - windows machine running the LabVIEW Server
-PORT = 2055 # The same port as used by the server - defined in LabVIEW
-global threadRunning # Used to stop threads
 threadRunning = False
-global s
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
@@ -57,25 +44,28 @@ os.system('clear')
 print('Connection with server established')
 
 try:
-	print(34 * '-')
-	print("        M A I N - M E N U")
-	print(' Press CTRL+C to close connection')
-	print(34 * '-')
-	# Create instance of class
-	threadRunning = True
-	transmit = Transmit()
-	receive = Receive()
-	# Start class
-	transmit.start()
-	receive.start()
-	while(threadRunning):
-		sleep(0.1)
+    print(34 * '-')
+    print("        M A I N - M E N U")
+    print(' Press CTRL+C to close connection')
+    print(34 * '-')
 
-except KeyboardInterrupt: # Stop program when CTRL+C is pressed
-	#print('Main stopped')
-	threadRunning = False
-	sleep(2)
-	s.close()
+    threadRunning = True
+
+    # Start receiving (keep this)
+    receive = Receive()
+    receive.start()
+
+    # OPTIONAL: start transmit thread if you want keyboard send
+    # transmit = Transmit()
+    # transmit.start()
+
+    while(threadRunning):
+        sleep(0.1)
+
+except KeyboardInterrupt:
+    threadRunning = False
+    sleep(1)
+    s.close()
 
 finally:
-	s.close()
+    s.close()
